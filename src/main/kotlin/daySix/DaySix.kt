51c5aa2ab89cedf2b8f7e47922730135cc33673b
Barry Moore <chiroptical@gmail.com>
2022-12-06 17:58:15 -0500

    Re-organization

--- src/main/kotlin/daySix/DaySix.kt
+++ src/main/kotlin/daySix/DaySix.kt
@@ -13,11 +13,17 @@ import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.useLines
 
-fun produceSets(path: Path, windowSize: Int = 4) = flow {
+// .indices is a nice method to describe something like `0 until s.length`
+fun searchMarkers(s: String, markerLength: Int = 4): Int =
+    s.indices.zip(s.windowed(markerLength, 1))
+        .first { (_, x) -> x.toSet().size == markerLength }
+        .first + markerLength
+
+fun produceSubsequences(path: Path, markerLength: Int = 4) = flow {
     path.useLines { lines ->
         val firstLine = lines.first()
-        for (idx in firstLine.indices) {
-            val endIdx = idx + windowSize
+        firstLine.indices.forEach { idx ->
+            val endIdx = idx + markerLength
             if (endIdx <= firstLine.length) {
                 emit(idx to firstLine.subSequence(idx, endIdx))
             }
@@ -25,43 +31,36 @@ fun produceSets(path: Path, windowSize: Int = 4) = flow {
     }
 }.flowOn(Dispatchers.IO)
 
-// .indices is a nice method to describe something like `0 until s.length`
-fun searchMarkers(s: String, markerLength: Int = 4): Int =
-    s.indices.zip(s.windowed(markerLength, 1))
-        .first { (_, x) -> x.toSet().size == markerLength }
-        .first + markerLength
-
 fun searchMarkersSet(p: Pair<Int, CharSequence>): Int? = run {
-    val charSet = mutableSetOf<Char>()
+    val set = mutableSetOf<Char>()
     // `Set<T>.add` returns a `Boolean` describing if it was added to the `Set<T>`.
     // `Collection<T>.all` requires every entry to be true **and** short-circuits.
-    if (p.second.all { charSet.add(it) }) {
+    if (p.second.all { set.add(it) }) {
         p.first
     } else {
         null
     }
 }
 
+suspend fun runSubsequence(path: Path, markerLength: Int) =
+    produceSubsequences(path, markerLength)
+        .first { searchMarkersSet(it) != null }
+        .first + markerLength
+
 @FlowPreview
 @ExperimentalCoroutinesApi
 suspend fun daySix() {
     val path = Path("inputFiles/daySix.txt")
+
+    // Original, non-`Flow` version. It uses `parMap` but no real parallel work
     val partOneResult = lines(path).parMap { searchMarkers(it, 4) }.toList()
-    val partTwoResult = lines(path).parMap { searchMarkers(it, 14) }.toList()
     println("Part 1: $partOneResult")
+    val partTwoResult = lines(path).parMap { searchMarkers(it, 14) }.toList()
     println("Part 2: $partTwoResult")
 
-    val partOneMarkerLength = 4
-    val partOneResultLazier =
-        produceSets(path, partOneMarkerLength)
-            .first { searchMarkersSet(it) != null }
-            .first + partOneMarkerLength
+    // `Flow` version with short-circuits
+    val partOneResultLazier = runSubsequence(path, 4)
     println("Lazier 1: $partOneResultLazier")
-
-    val partTwoMarkerLength = 14
-    val partTwoResultLazier =
-        produceSets(path, partTwoMarkerLength)
-            .first { searchMarkersSet(it) != null }
-            .first + partTwoMarkerLength
+    val partTwoResultLazier = runSubsequence(path, 14)
     println("Lazier 2: $partTwoResultLazier")
 }
